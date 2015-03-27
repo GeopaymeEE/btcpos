df7a8672b5c9d2aa41e1e8dd16f730808fbbe794
Adonis Valamontes <tony@geopay.me>
2015-03-27 19:45:38 +0200

    update

---
+++ config/config.js
@@ -0,0 +1,8 @@
+(function() {
+  module.exports.secret = 'weareallmadeofstars';
+
+  module.exports.sendgrid_user = 'geopayme';
+
+  module.exports.sendgrid_password = 'A!g30peYm3';
+
+}).call(this);
--- config/config.js.sample
+++
@@ -1,8 +0,0 @@
-(function() {
-  module.exports.secret = 'weareallmadeofstars';
-
-  module.exports.sendgrid_user = 'user';
-
-  module.exports.sendgrid_password = 'password';
-
-}).call(this);
